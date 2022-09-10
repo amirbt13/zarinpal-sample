258edcac7d6a4f0ca798cbfb116084177da26ada
Amir Basirati <amirbasirati13@gmail.com>
2022-09-10 12:56:40 +0430

    Yekan font installed and html lang changed to farsi/two components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// Components
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+        <Navbar />
+        <Banner />
     </div>
   );
 }
