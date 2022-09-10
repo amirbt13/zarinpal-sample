895583d081b2138ae1ff17f7cf8d0a309116f25c
Amir Basirati <amirbasirati13@gmail.com>
2022-09-10 14:47:42 +0430

    navbar and banner ok shod

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='banner'>
+        <div className='banner-text'>
+            <h2>بیش از ۱۲ سال
+                <br/>
+                انتخاب مطمئنِ کسب و کارهای آنلاین
+            </h2>
+            <p>زرین‌پال، اولین پرداخت‌یار پیشگام کشور، با به‌کارگیری سبک و استانداردهای جدید در ارائه‌ی خدمات درگاه پرداخت اینترنتی، سرویس‌های متنوعی در حوزه‌ی پرداخت الکترونیک را برای کسب و کارها ارائه کرده است؛ با این هدف که در افزایش سهم تجارت الکترونیکی در تولید ناخالص ملی و کمک به رشد و توسعه‌ی کسب‌وکارها، نقش سازنده‌‌ای ایفا کند.
+
+        </p>
+            <button className='signup-btn'>ثبت نام</button>
+            <button className='login-btn'>ورود</button>
+        </div>
+        <div className='banner-img'>
+
+        </div>
+
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import logo from '../images/favicon.ico'
+
 const Navbar = () => {
   return (
-    <div>محصولات</div>
+    <div className='navbar'>
+        <div className='title'>
+            <img className='logo' src={logo} alt="zarin-logo"/>
+            <h4>زرین پال</h4>
+        </div> 
+        <div className='options'>
+            <ul>
+                <li>محصولات</li>
+                <li>تعرفه ها</li>
+                <li>توسعه دهنده ها</li>
+                <li>تماس با ما</li>
+                <li>بیشتر</li>
+            </ul>
+            <button>زرین پال من</button>
+        </div>
+    </div>
   )
 }
 
